23412d5a14b6c27bae7f16d4587d9cdff2a86da1
나성 <53929558+narasung101@users.noreply.github.com>
2020-01-17 18:43:45 +0900

    0117
    0117

---
+++ src/main/webapp/resources/js/nvue/customer_vue.js
@@ -0,0 +1,160 @@
+"use strict";
+var customer_vue = customer_vue || {}
+customer_vue = {
+		
+		custMain : ()=>{
+			return `
+			    
+			<head>
+				  <meta charset="utf-8">
+				  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+				  <meta name="description" content="">
+				  <meta name="author" content="">
+				
+				  <title>Cust Service</title>
+				
+				  <!-- Bootstrap core CSS -->
+				  <link href="https://blackrockdigital.github.io/startbootstrap-heroic-features/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+				
+				  <!-- Custom styles for this template -->
+				 <link href="https://blackrockdigital.github.io/startbootstrap-heroic-features/css/heroic-features.css" rel="stylesheet">
+				 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+			</head>
+
+			<body>
+			
+			
+				  <style type="text/css">
+				      .jumbotron{
+				        background-image:url('https://blogifs-endpoint.azureedge.net/wp-content/uploads/2018/10/RANVP620974266Easyturn-min-1116x600.jpg');
+				        background-size: cover;
+				        text-shadow: black 0.4em 0.4em 0.4em;
+				        color:white;
+				      } 		  				
+				    				    
+				      </style>
+				      
+				  <!-- Navigation -->
+				  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+				    <div class="container" >
+				      <a class="navbar-brand" href="#">Good For</a>
+				      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				        <span class="navbar-toggler-icon"></span>
+				      </button>
+				      <div class="collapse navbar-collapse" id="navbarResponsive">
+				        <ul class="navbar-nav ml-auto">
+				          <li class="nav-item active">
+				            <a class="nav-link" href="#">Home
+				              <span class="sr-only">(current)</span>
+				            </a>
+				          </li>
+				          <li class="nav-item">
+				            <a class="nav-link" href="#">About</a>
+				          </li>
+				          <li class="nav-item">
+				            <a class="nav-link" href="#">로그인</a>
+				          </li>
+				          <li class="nav-item">
+				            <a class="nav-link" href="#">회원가입</a>
+				          </li>
+				        </ul>
+				      </div>
+				    </div>
+				  </nav>
+				
+				  <!-- Page Content -->
+				  <div class="container" style="margin-top: 24px;">
+				
+				    <!-- Jumbotron Header -->
+				    <div class="jumbotron">
+				      <h1 class="display-3">굿포</h1>
+				      <p class="lead">고객 감동과 만족을 위해 노력하는 굿포 고객센터입니다.</p>
+				      <a href="#" class="btn btn-primary btn-lg">바로가기!</a>
+				    </div>
+				
+				    <!-- Page Features -->
+				    
+				  			
+				
+    
+	<div class="row text-center">
+				
+				      <div class="col-lg-3 col-md-6 mb-4">
+				        <div class="card h-100">
+				          <img class="card-img-top" src="https://image.flaticon.com/icons/svg/1660/1660114.svg" alt="" class="img-fluid">
+				          <div class="card-body">
+				            <h4 class="card-title">자주하는질문</h4>
+				            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+				          </div>
+				          <div class="card-footer">
+				            <a href="#" class="btn btn-primary">Find Out More!</a>
+				          </div>
+				        </div>
+				      </div>
+				
+				      <div class="col-lg-3 col-md-6 mb-4">
+				        <div class="card h-100">
+				        <img class="card-img-top" src="https://image.flaticon.com/icons/svg/561/561127.svg" alt="">
+				           <div class="card-body">
+				            <h4 class="card-title">문의/제안</h4>
+				            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+				          </div>
+				          <div class="card-footer">
+				            <a href="#" class="btn btn-primary">Find Out More!</a>
+				          </div>
+				        </div>
+				      </div>
+				
+				      <div class="col-lg-3 col-md-6 mb-4">
+				        <div class="card h-100">
+				          <img class="card-img-top" src="https://image.flaticon.com/icons/svg/1069/1069159.svg" alt="">
+				          <div class="card-body">
+				            <h4 class="card-title">고객게시판</h4>
+				            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+				          </div>
+				          <div class="card-footer">
+				            <a href="#" class="btn btn-primary">Find Out More!</a>
+				          </div>
+				        </div>
+				      </div>
+				      
+				       <div class="col-lg-3 col-md-6 mb-4">
+				        <div class="card h-100">
+				          <img class="card-img-top" src="https://image.flaticon.com/icons/svg/482/482598.svg" alt="">
+				          <div class="card-body">
+				            <h4 class="card-title">공지사항</h4>
+				            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+				          </div>
+				          <div class="card-footer">
+				            <a href="#" class="btn btn-primary">Find Out More!</a>
+				          </div>
+				        </div>
+				      </div>   		      
+				      
+				
+				    </div>
+				    
+
+			
+				    <!-- /.row -->
+				
+				  </div>
+				  <!-- /.container -->
+				
+				  <!-- Footer -->
+				  <footer class="py-5 bg-dark">
+				    <div class="container">
+				      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+				    </div>
+				    <!-- /.container -->
+				  </footer>
+				
+				  <!-- Bootstrap core JavaScript -->
+				  <script src="https://blackrockdigital.github.io/startbootstrap-heroic-features/vendor/jquery/jquery.min.js"></script>
+				  <script src="/web/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+			</body>
+			
+			`
+		} 
+}
